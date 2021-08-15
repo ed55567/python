80ed406e1a6b0de4058a2d9629a86adb577b95e9
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 16:11:46 -0400

    Update functions.py
    added parameters and  arguments

--- functions.py
+++ functions.py
@@ -6,3 +6,27 @@ def directions_to_timesSq():
 
 
 directions_to_timesSq()
+
+# calling function
+print("Checking the weather for you!")
+
+
+def weather_check():
+    print("Looks great outside! Enjoy your trip.")
+
+
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+
+weather_check()
+
+# Parameters & Arguments
+
+
+def generate_trip_instructions(location):
+    print("Looks like you are planning a trip to visit " + location)
+    print("You can use the public subway system to get to " + location)
+
+
+# Checkpoint 3 & 4
+generate_trip_instructions("Central Park")
+generate_trip_instructions("Grand Central Station")
