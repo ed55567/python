6dd68a94afac38be88690b22be4239d3a48721fd
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 14:25:00 -0400

    Update loops.py
    added loops to scoops sold

--- loops.py
+++ loops.py
@@ -55,3 +55,17 @@ for age in ages:
     if age < 21:
         continue
     print(age)
+
+
+# Nested Loops
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+
+scoops_sold = 0
+
+for location in sales_data:
+    print(location)
+    for element in location:
+        scoops_sold += element
+
+print(scoops_sold)
