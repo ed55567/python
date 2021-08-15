1d78b643630442f7c8604e62d9fa865949a5e33f
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 15:40:51 -0400

    Create functions.py
    creating 1st python function

---
+++ functions.py
@@ -0,0 +1,8 @@
+# function
+def directions_to_timesSq():
+    print("Walk 4 mins to 34th St Herald Square train station")
+    print("Take the Northbound N, Q, R, or W train 1 stop")
+    print("Get off the Times Square 42nd Street stop")
+
+
+directions_to_timesSq()
