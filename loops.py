3a7c77202694e240800ed44e2eab5367146e196b
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 15:10:27 -0400

    Update loops.py
    added list  Comprehensions: Conditionals

--- loops.py
+++ loops.py
@@ -69,3 +69,11 @@ for location in sales_data:
         scoops_sold += element
 
 print(scoops_sold)
+
+
+# List Comprehensions: Conditionals
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+
+can_ride_coaster = [height for height in heights if height > 161]
+
+print(can_ride_coaster)
