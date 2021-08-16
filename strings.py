8aee1f86ecc9f4983e9d0c5bbb013f29185cc15f
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 22:29:45 -0400

    Update strings.py
    added strings and conditionals

--- strings.py
+++ strings.py
@@ -19,3 +19,84 @@ def account_generator(first_name, last_name):
 new_account = account_generator(first_name, last_name)
 
 print(new_account)
+
+
+# More and More String Slicing
+first_name = "Reiko"
+last_name = "Matsuki"
+
+
+def password_generator(first_name, last_name):
+    temp_password = first_name[len(first_name)-3:] + \
+        last_name[len(last_name)-3:]
+    return temp_password
+
+
+temp_password = password_generator(first_name, last_name)
+
+print(temp_password)
+
+
+first_name = "Bob"
+last_name = "Daily"
+
+# first_name[0] = "R"
+
+fixed_first_name = "R" + first_name[1:]
+
+
+# Strings are Immutable
+
+first_name = "Bob"
+last_name = "Daily"
+
+# first_name[0] = "R"
+
+fixed_first_name = "R" + first_name[1:]
+
+print(fixed_first_name)
+
+
+def get_length(word):
+    counter = 0
+    for letter in word:
+        counter += 1
+    return counter
+
+
+def letter_check(word, letter):
+    for character in word:
+        if character == letter:
+            return True
+    return False
+
+
+def contains(big_string, little_string):
+    return little_string in big_string
+
+
+def common_letters(string_one, string_two):
+    common = []
+    for letter in string_one:
+        if (letter in string_two) and not (letter in common):
+            common.append(letter)
+    return common
+
+
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+
+
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i-1]
+    return password
