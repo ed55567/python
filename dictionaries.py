9045b3213f5a30f666abdf1fbb43ef454d248e12
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-16 16:58:55 -0400

    Create dictionaries.py
     added dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+sensors = {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+
+
+# Make a Dictionary
+
+translations = {"mountain": "orod", "bread": "bass",
+                "friend": "mellon", "horse": "roch"}
+
+
+children = {"von Trapp": ["Johannes", "Rosmarie",
+                          "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+
+animals_in_zoo = {}
+animals_in_zoo['zebras'] = 8
+animals_in_zoo['monkeys'] = 12
+animals_in_zoo['dinosaurs'] = 0
+print(animals_in_zoo)
+
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
