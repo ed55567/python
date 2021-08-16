b99df6d0baf585a10fa6ae38f1c0af382e3e1df1
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-08-15 21:49:11 -0400

    Update strings.py
    added  Concatenating Strings

--- strings.py
+++ strings.py
@@ -4,3 +4,18 @@ last_name = "Villanueva"
 new_account = last_name[:5]
 
 temp_password = last_name[2:6]
+
+# Concatenating Strings
+
+first_name = "Julie"
+last_name = "Blevins"
+
+
+def account_generator(first_name, last_name):
+    account_name = first_name[:4] + last_name[:5]
+    return account_name
+
+
+new_account = account_generator(first_name, last_name)
+
+print(new_account)
